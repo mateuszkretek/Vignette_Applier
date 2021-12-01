de2583a8dd0e2fbac6dc01a1bb5f34b6882c533a
Mateusz <mkretek27@gmail.com>
2021-12-01 11:01:50 +0100

    test

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def main():
+	img = cv2.imread('temp.jpg', 0)
+	rows, cols = img.shape
+	a = cv2.getGaussianKernel(cols, 300)
+	b = cv2.getGaussianKernel(rows, 300)
+	c = b*a.T
+	d = c/c.max()
+	e = img*d
+	cv2.imwrite('vig2.png', e)
+	return
